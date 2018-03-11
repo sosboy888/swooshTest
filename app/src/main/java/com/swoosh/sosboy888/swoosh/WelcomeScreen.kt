6cf5442ec707753ff3c3379bc01b93723f8f4bd3
sosboy888 <swastiksharma88@gmail.com>
2018-03-11 18:53:06 +0530

    transition between 1st and 2nd

--- app/src/main/java/com/swoosh/sosboy888/swoosh/WelcomeScreen.kt
+++ app/src/main/java/com/swoosh/sosboy888/swoosh/WelcomeScreen.kt
@@ -1,12 +1,21 @@
 package com.swoosh.sosboy888.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome_screen.*
 
 class WelcomeScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_screen)
+        getStartedButton.setOnClickListener(){
+            val leagueIntent=Intent(this,leagueSelection::class.java)
+            startActivity(leagueIntent)
+
+        }
+
+
     }
 }
